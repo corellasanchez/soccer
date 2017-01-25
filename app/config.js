82864e719cf3f225b9e09567c3a4ea16964d5eec
Roy Corella Sanchez <corellasanchez@hotmail.com>
2017-01-25 03:40:26 -0600

    Add config

---
+++ app/config.js
@@ -0,0 +1,10 @@
+(function() {
+  'use strict';
+   angular
+    .module('app')
+    .constant('env', {
+      apiKey: 'd9b2c29baac94818a4908116a55d6f08',
+      apiUrl: 'http://api.football-data.org/v1/'
+    });
+})();
+//
